5cb3207e3d6957d77b6ff106857028d90f4d6347
Zubi7Zubair <138170071+Zubi7Zubair@users.noreply.github.com>
2024-05-09 18:56:07 +0500

    Initial commit

---
+++ lib/navigation_sidebar.dart
@@ -0,0 +1,3 @@
+library navigation_sidebar;
+
+export "src/navigation_sidebar.dart";
---
+++ lib/src/navigation_sidebar.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NavigationSideBar extends StatelessWidget {
+  const NavigationSideBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Drawer(),
+    );
+  }
+}
